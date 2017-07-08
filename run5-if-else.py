58937995d13e35f72454f9af12aa02217d17d935
Baranq <baranq@gmail.com>
2017-07-08 19:40:06 +0200

    Add files via upload

---
+++ run5-if-else.py
@@ -0,0 +1,17 @@
+#Petla if / else
+# porównanie x == y
+# nierówne x != y
+# większe x > y    wieksze lub rowne   x >= y
+# mniejsze x < y   mniejsze lub rowne  x <= y
+
+
+
+x = 67
+#x = 5
+
+if x == 5:
+    print ("Zgadza się !")
+else:
+    print ("NIE ZGADZA SIĘ")
+
+print("Gotowe :)")
