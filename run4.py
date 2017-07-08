7e1eee941d8a80d9d4b8f3e8509839bbac5ddc3f
Baranq <baranq@gmail.com>
2017-07-08 19:12:49 +0200

    Add files via upload

---
+++ run4.py
@@ -0,0 +1,17 @@
+x = "Ala ma kota, a kot ma Alę !"
+
+print (x)
+
+
+y = x[:6]
+print (y)
+print (type(y))
+
+z = x[7:]
+print (z)
+print (type(z))
+
+#wypisywanie ostatniego znaku
+z = x[-1]
+print (z)
+print (type(z))
