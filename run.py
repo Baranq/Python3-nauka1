f35075f1d83aebdd60b9334877181bdcd95607a9
Baranq <baranq@gmail.com>
2017-07-08 19:12:32 +0200

    Add files via upload

---
+++ run.py
@@ -0,0 +1,40 @@
+
+y = "JESTEM ZAJEBISTY BO UMIE PROGRAMOWAC w PYTHONIE :P "
+print (y)
+print (type(y))
+
+x = 10
+
+print (x)
+
+x = x + 2
+
+print (x)
+print(type(x))
+
+x = x / 2
+print(x)
+print(type(x))
+
+print ("WPISZ LICZBĘ CAŁKOWITA:")
+x = input()
+
+
+x = int(x)
+
+x = x + 2
+
+print (x)
+print(type(x))
+
+
+print ("WPISZ LICZBĘ ZMIENNOPRZECINKOWĄ:")
+x = input()
+
+
+x = float(x)
+
+x = x + 3
+
+print (x)
+print(type(x))
